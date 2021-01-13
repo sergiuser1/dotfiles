8f38794fa7bdcba9741188cf80897760d456e8bb
sergiuser1 <serge.ojog@gmail.com>
2021-01-13 01:51:02 +0100

    Remove .vim folder

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -3,48 +3,5 @@
 directory=$(dirname $(pwd))
 echo $directory
 
-echo ${BASH_SOURCE[0]}
-relpath() {
-    # both $1 and $2 are absolute paths beginning with /
-    # $1 must be a canonical path; that is none of its directory
-    # components may be ".", ".." or a symbolic link
-    #
-    # returns relative path to $2/$target from $1/$source
-    source=$1
-    target=$2
-
-    common_part=$source
-    result=
-
-    while [ "${target#"$common_part"}" = "$target" ]; do
-        # no match, means that candidate common part is not correct
-        # go up one level (reduce common part)
-        common_part=$(dirname "$common_part")
-        # and record that we went back, with correct / handling
-        if [ -z "$result" ]; then
-            result=..
-        else
-            result=../$result
-        fi
-    done
-
-    if [ "$common_part" = / ]; then
-        # special case for root (no common path)
-        result=$result/
-    fi
-
-    # since we now have identified the common part,
-    # compute the non-common part
-    forward_part=${target#"$common_part"}
-
-    # and now stick all parts together
-    if [ -n "$result" ] && [ -n "$forward_part" ]; then
-        result=$result$forward_part
-    elif [ -n "$forward_part" ]; then
-        # extra slash removal
-        result=${forward_part#?}
-    fi
-
-    printf '%s\n' "$result"
-}
-relpath "$directory" "$HOME"
+# find magic
+find . -maxdepth 2 -not -name '*work' -name '.git*' -prune -o -name 'misc' -prune -o -print
