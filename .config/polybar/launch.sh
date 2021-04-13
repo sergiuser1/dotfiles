054c423b0d4892f0872ed1610b7511d51b7e981d
sergiuser1 <sergiu.ojog@protonmail.com>
2021-04-13 20:37:10 +0200

    Fix polybar stacking on bpswm

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -5,8 +5,8 @@ killall -q polybar
 
 # Start polybar
 if pgrep -x bspwm; then
-    polybar --reload -c ~/.config/polybar/config_bspwm.ini main &
+    polybar --reload -c ~/.config/polybar/config_bspwm.ini main
 else
-    polybar --reload -c ~/.config/polybar/config.ini main &
+    polybar --reload -c ~/.config/polybar/config.ini main
 fi
 
