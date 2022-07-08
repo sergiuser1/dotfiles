5e48b394eafdd7b4cd90295e535e440ab7b10814
sergiuser1 <sergiu.ojog@protonmail.com>
2022-07-08 13:34:07 +0200

    Improve polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,12 +1,4 @@
 #!/usr/bin/env sh
 
-# Kill running instances
-killall -q polybar
-
-# Start polybar
-if pgrep -x bspwm; then
-    polybar --reload -c ~/.config/polybar/config_bspwm.ini main
-else
-    polybar --reload -c ~/.config/polybar/config.ini main
-fi
-
+killall -q polybar 
+polybar -l warning -r -c $HOME/.config/polybar/config.ini main
