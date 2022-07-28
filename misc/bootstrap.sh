aeebb55b31dec0d23388607de984981dcc999aca
sergiuser1 <sergiu.ojog@protonmail.com>
2022-07-28 13:53:43 +0200

    Add host specific configs

--- misc/bootstrap.sh
+++ misc/bootstrap.sh
@@ -1,20 +1,39 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Check if we are in the 'dotfiles' folder
 [ ${PWD##*/} = 'dotfiles' ] || exit 1
 
-# find magic
-find  -name '*git*' -prune -o -name 'misc' -prune -o -not -name '*.work' \
+HOSTNAME="$(hostname -s)"
+
+create_links() {
+
+    # find magic
+    find -name '*git*' -prune -o \
+    -name 'misc' -prune -o \
+    -name 'desks' -prune -o \
+    -name 'sinkpad' -prune -o \
     -type f -print | while read file; do
 
-    # Remove './' from the beginning
-    file="${file#./}"
-    # Make sure the folders exist
-    mkdir -p "$(dirname "$HOME/$file")"
+        # Remove './' from the beginning
+        file="${file#./}"
+        # Make sure the folders exist
+        mkdir -p "$(dirname "$HOME/$file")"
 
-    # ln makes backups even the file is a symlink
-    if [[ ! -L "$HOME/$file" ]]; then
-        ln -rsfv --backup=simple "$file" "$HOME/$file"
-    fi
+        # ln makes backups even the file is a symlink
+        if [[ ! -L "$HOME/$file" ]]; then
+            ln -rsfv --backup=simple "$file" "$HOME/$file"
+        fi
+    done
+}
 
-done
+# Create base links
+create_links
+
+# Create host specific links
+if [ $HOSTNAME == "desks" ]; then
+    cd desks/
+    create_links
+elif [ $HOSTNAME == "sinkpad" ]; then
+    cd sinkpad/
+    create_links
+fi
