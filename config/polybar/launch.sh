c0556c73ee7a22acc159a37d60a30134d0a08d2d
sergiuser1 <sergiu.ojog@tuta.io>
2022-12-23 20:34:24 +0100

    Make polybar launch script better

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -4,10 +4,11 @@ killall -q polybar
 
 HOST="$(hostname -s)"
 
-if [ "$HOST" = 'sinkpad' ]; then
-    polybar -l warning -r -c $HOME/.config/polybar/main.ini thinkpad
-elif [ "$HOST" = 'desks' ]; then
-    polybar -l warning -r -c $HOME/.config/polybar/main.ini desks
-else
-    polybar -l warning -r -c $HOME/.config/polybar/main.ini thinkpad
-fi
+case "$HOST" in
+    sinkpad|desks)
+        BAR="$HOST";;
+    *)
+        BAR=default;;
+esac
+
+polybar -l warning -r -c $HOME/.config/polybar/main.ini "$BAR"
