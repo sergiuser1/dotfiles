b65672d22d09b297b3327890aefb1cceeae9d1c2
sergiuser1 <sergiu.ojog@tuta.io>
2023-11-17 14:22:59 +0100

    Add ripgrep config

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -573,12 +573,13 @@ local null_ls = require("null-ls")
 null_ls.setup({
   sources = {
     null_ls.builtins.formatting.stylua,
-    null_ls.builtins.code_actions.eslint_d,
-    null_ls.builtins.diagnostics.eslint_d,
+    null_ls.builtins.code_actions.eslint,
+    null_ls.builtins.diagnostics.eslint,
     null_ls.builtins.completion.spell,
   },
 })
 
+require'lspconfig'.angularls.setup{}
 
 -- Comment stuff
 local function comment_post_hook()
@@ -600,6 +601,8 @@ require('Comment').setup({
     ---Line-comment keymap
     line = '<C-_>',
   },
+  -- Ignore empty lines
+  ignore = '^$',
   post_hook = comment_post_hook,
 })
 
---
+++ config/ripgrep/ripgrep.sh
@@ -0,0 +1,8 @@
+# Search hidden files / directories
+--hidden
+
+# Ignore '.git' folder
+--glob=!.git/*
+
+# No one cares about case
+--smart-case
